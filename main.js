0fbedaaf5eef99946b842771f3c02297ddbc8faa
hessennasser <92742551+hessennasser@users.noreply.github.com>
2022-12-12 11:28:25 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -11,3 +11,32 @@ function myFunction() {
     menuIcon.classList.toggle("active");
     navMenu.classList.toggle("active");
 }
+
+// start btn-up 
+btnUp = document.querySelector('.up-btn');
+
+window.addEventListener('scroll', function () {
+    if (window.scrollY > 600) {
+        btnUp.classList.add("active");
+    } else {
+        btnUp.classList.remove("active");
+    }
+});
+btnUp.addEventListener("click", function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+});
+// end btn-up 
+
+// start btn-up 
+whatsBtn = document.querySelector('.whats-btn');
+window.addEventListener('scroll', function () {
+    if (window.scrollY > 600) {
+        whatsBtn.classList.add("active");
+    } else {
+        whatsBtn.classList.remove("active");
+    }
+});
+// end btn-up 
